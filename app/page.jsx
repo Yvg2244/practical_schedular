cb422256d17aa67492dfc9e11016a8492f918b71
Yvg2244 <yvgupta24@gmail.com>
2023-05-20 23:49:44 +0530

    fixed validation,duplicate text

--- app/page.jsx
+++ app/page.jsx
@@ -2,19 +2,9 @@
 import React, { useEffect, useState } from "react";
 import { useFormik } from "formik";
 import * as XLSX from "xlsx";
-import arrayShiftFunction from "../components/arrayShiftFunction";
-import { modifyArrayFunction } from "../components/modifyArrayFunction";
-import createBatch from "../components/createBatch";
-import tableGenerator from "../components/tableGenerator";
 import axios from "axios";
 
 const page = () => {
-  function objectMap(object, mapFn) {
-    return Object.keys(object).reduce(function (result, key) {
-      result[key] = mapFn(object[key]);
-      return result;
-    }, {});
-  }
   const shortenArr = (arr) => {
     var modifiedArr = "";
     var count = 1;
@@ -36,6 +26,18 @@ const page = () => {
     }
     return modifiedArr;
   };
+  const validate = (values) => {
+    const errors = {};
+    if (!values.uploadsemester) errors.uploadsemester = "Required";
+    if (!values.semester) errors.semester = "Required";
+    if (!values.date) errors.date = "Required";
+    if (!values.subject) errors.subject = "Required";
+    if (!values.externalId) errors.externalId = "Required";
+    if (!values.batch_size) errors.batch_size = "Required";
+    if (!values.get_schedule_sem) errors.get_schedule_sem = "Required";
+    if (!values.reset_schedule_sem) errors.reset_schedule_sem = "Required";
+    return errors;
+  };
   const formik = useFormik({
     initialValues: {
       uploadsemester: "",
@@ -46,8 +48,22 @@ const page = () => {
       externalId: "",
       subject: "",
       get_schedule_sem: "",
+      reset_schedule_sem: "",
     },
+    validate,
   });
+  var prevSub = "";
+  var showSub = "";
+  var prevDate = "";
+  var showDate = "";
+  var prevInvig = "";
+  var showInvig = "";
+  const [createScheduleErr, setCreateScheduleErr] = useState(false);
+  const [getScheduleErr, setGetScheduleErr] = useState(false);
+  const [resetScheduleErr, setResetScheduleErr] = useState(false);
+  const [createScheduleMsg, setCreateScheduleMsg] = useState("");
+  const [getScheduleMsg, setGetScheduleMsg] = useState("");
+  const [resetScheduleMsg, setResetScheduleMsg] = useState("");
   const [excelFile, setExcelFile] = useState(null);
   const [excelFileErr, setExcelFileErr] = useState(null);
   const [fileData, setFileData] = useState(null);
@@ -57,34 +73,7 @@ const page = () => {
   ];
   const [displayTable, setDisplayTable] = useState(null);
   useEffect(() => {
-    console.log(displayTable);
-
-    // if (displayTable) {
-    //   Object.keys(displayTable).forEach((key) => {
-    //     var subject = key;
-    //     var subjectData = displayTable[key];
-    //     console.log(subject);
-    //     Object.keys(subjectData).forEach((key) => {
-    //       var subjectDate = key;
-    //       console.log(subjectDate);
-    //       var subjectDatePracticals = subjectData[subjectDate];
-    //       Object.keys(subjectDatePracticals).forEach((key) => {
-    //         var inviglatorId = key;
-    //         console.log(inviglatorId);
-    //         var studentsOnInviglator = subjectDatePracticals[inviglatorId];
-
-    //         Object.keys(studentsOnInviglator).forEach((key) => {
-    //           var batch = key;
-    //           console.log(batch);
-    //           var studentInBatch = studentsOnInviglator[key];
-    //           studentInBatch.map((item) => {
-    //             console.log(item);
-    //           });
-    //         });
-    //       });
-    //     });
-    //   });
-    // }
+    // console.log(displayTable);
   }, [displayTable]);
 
   useEffect(() => {
@@ -117,14 +106,33 @@ const page = () => {
     }
   }, [excelFile]);
 
+  useEffect(() => {
+    console.log(createScheduleErr, getScheduleErr, resetScheduleErr);
+    console.log(formik.errors);
+    if (
+      formik.errors.hasOwnProperty("semester") ||
+      formik.errors.hasOwnProperty("date") ||
+      formik.errors.hasOwnProperty("subject") ||
+      formik.errors.hasOwnProperty("externalId") ||
+      formik.errors.hasOwnProperty("batch_size")
+    )
+      setCreateScheduleErr(true);
+    else setCreateScheduleErr(false);
+    if (formik.errors.hasOwnProperty("get_schedule_sem"))
+      setGetScheduleErr(true);
+    else setGetScheduleErr(false);
+    if (formik.errors.hasOwnProperty("reset_schedule_sem"))
+      setResetScheduleErr(true);
+    else setResetScheduleErr(false);
+  }, [formik.errors]);
   const generateCLicked = (e) => {
     // console.log(formik.values);
     axios
       .post("https://scheduler-b3ns.onrender.com/get", {
-        sem:formik.values.get_schedule_sem
+        sem: formik.values.get_schedule_sem,
       })
       .then((res) => {
-        console.log(res.data);
+        console.log(res);
         setDisplayTable(res.data);
       })
       .catch((err) => {
@@ -162,9 +170,15 @@ const page = () => {
   };
 
   return (
-    <div className="w-full h-full my-[1rem] flex flex-col items-center justify-center">
-      <h4 className="my-[3rem]">Practical Schedular</h4>
-      <div className="grid gap-4 grid-cols-2">
+    <div className="w-full h-full px-4 my-[1rem] flex flex-col items-center justify-center">
+      <h4 className="mb-[3rem] mt-[1rem] text-2xl font-extrabold ">
+        Practical Schedular
+      </h4>
+      <div className="grid gap-4 px-4 grid-cols-2">
+        <h4 className="col-span-2 text-xl font-semibold text-center">
+          Upload student data
+        </h4>
+
         <label htmlFor="uploadsemester">Enter semester of data</label>
         <input
           onChange={formik.handleChange}
@@ -174,8 +188,16 @@ const page = () => {
           id="uploadsemester"
           label="uploadsemester"
           placeholder="Enter semsester"
+          onBlur={formik.handleBlur}
         />
-
+        <div></div>
+        <div>
+          {formik.touched.uploadsemester && formik.errors.uploadsemester ? (
+            <div className="text-red-600 text-md">
+              {formik.errors.uploadsemester}
+            </div>
+          ) : null}
+        </div>
         <label htmlFor="file" className="justify-cente flex items-center">
           Upload Student File
         </label>
@@ -202,7 +224,10 @@ const page = () => {
             }
           }}
         />
-        <h4 className="col-span-2 text-center">Create Schedule</h4>
+        <div className="w-full h-1 border-b-gray-700 my-1 border-[1px] col-span-2"></div>
+        <h4 className="col-span-2 text-xl font-semibold text-center">
+          Create Schedule
+        </h4>
 
         <label htmlFor="semester">Enter Semester</label>
         <input
@@ -212,7 +237,14 @@ const page = () => {
           onChange={formik.handleChange}
           value={formik.values.name}
           type="text"
+          onBlur={formik.handleBlur}
         />
+        <div></div>
+        <div>
+          {formik.touched.semester && formik.errors.semester ? (
+            <div className="text-red-600 text-md">{formik.errors.semester}</div>
+          ) : null}
+        </div>
         <label htmlFor="date">Enter Date</label>
         <input
           id="date"
@@ -222,7 +254,14 @@ const page = () => {
           onChange={formik.handleChange}
           value={formik.values.name}
           type="text"
+          onBlur={formik.handleBlur}
         />
+        <div></div>
+        <div>
+          {formik.touched.date && formik.errors.date ? (
+            <div className="text-red-600 text-md">{formik.errors.date}</div>
+          ) : null}
+        </div>
         <label htmlFor="subject">Enter Subject</label>
         <input
           id="subject"
@@ -231,8 +270,14 @@ const page = () => {
           onChange={formik.handleChange}
           value={formik.values.name}
           type="text"
+          onBlur={formik.handleBlur}
         />
-
+        <div></div>
+        <div>
+          {formik.touched.subject && formik.errors.subject ? (
+            <div className="text-red-600 text-md">{formik.errors.subject}</div>
+          ) : null}
+        </div>
         <label className="justify-cente flex items-center">Inviglator Id</label>
         <input
           id="externalId"
@@ -241,7 +286,16 @@ const page = () => {
           type="number"
           onChange={formik.handleChange}
           value={formik.values.name}
+          onBlur={formik.handleBlur}
         />
+        <div></div>
+        <div>
+          {formik.touched.externalId && formik.errors.externalId ? (
+            <div className="text-red-600 text-md">
+              {formik.errors.externalId}
+            </div>
+          ) : null}
+        </div>
         <label htmlFor="batch_size" className="justify-cente flex items-center">
           Students under external
         </label>
@@ -253,11 +307,27 @@ const page = () => {
           id="batch_size"
           label="batch_size"
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
         />
-        <button className="col-span-2" onClick={createCLicked}>
+        <div></div>
+        <div>
+          {formik.touched.batch_size && formik.errors.batch_size ? (
+            <div className="text-red-600 text-md">
+              {formik.errors.batch_size}
+            </div>
+          ) : null}
+        </div>
+        <button
+          className="col-span-2 w-[15rem] items-center rounded-md p-2 font-semibold tracking-wider"
+          onClick={createCLicked}
+          disabled={createScheduleErr}
+        >
           Create Schedule
         </button>
-        <h4 className="col-span-2 text-center">Get Schedule</h4>
+        <div className="w-full h-1 border-b-gray-700 my-1 border-[1px] col-span-2"></div>
+        <h4 className="col-span-2 text-xl font-semibold text-center">
+          Get Schedule
+        </h4>
 
         <label className="justify-cente flex items-center">
           Enter Semester
@@ -269,45 +339,70 @@ const page = () => {
           type="number"
           onChange={formik.handleChange}
           value={formik.values.name}
+          onBlur={formik.handleBlur}
         />
-        <button className="col-span-2" onClick={generateCLicked}>
+        <div></div>
+        <div>
+          {formik.touched.get_schedule_sem && formik.errors.get_schedule_sem ? (
+            <div className="text-red-600 text-md">
+              {formik.errors.get_schedule_sem}
+            </div>
+          ) : null}
+        </div>
+        <button
+          className="col-span-2 w-[15rem] items-center rounded-md p-2 font-semibold tracking-wider"
+          onClick={generateCLicked}
+          disabled={getScheduleErr}
+        >
           Get Schedule
         </button>
-        {/* <div className="grid grid-cols-5 w-[100vw] items-center gap-4 px-5">
-          <p>Subject Code</p>
-          <p>Date</p>
-          <p>Inviglator Id</p>
-          <p>Slot</p>
-          <p>Students</p>
-        </div> */}
+
         <div className="flex flex-col w-[80vw] justify-center items-center gap-[2rem]">
           {displayTable ? (
             Object.keys(displayTable).map((item) => {
               var subject = item;
+              var curSub = subject;
               var subjectData = displayTable[item];
-              // return <span>{subject}</span>;
+
               return Object.keys(subjectData).map((key) => {
                 var subjectDate = key;
                 var subjectDatePracticals = subjectData[subjectDate];
-                // console.log(subjectDate);
+                var curDate=subjectDate
                 return Object.keys(subjectDatePracticals).map((key) => {
                   var inviglatorId = key;
-
                   var studentsOnInviglator =
                     subjectDatePracticals[inviglatorId];
-                  // console.log(inviglatorId);
-                  // counter
+                  var curInvig=inviglatorId
                   return Object.keys(studentsOnInviglator).map((key) => {
                     var batch = key;
                     var studentInBatch = studentsOnInviglator[key];
                     var arrOfStudents = shortenArr(studentInBatch);
-
+                    if (curSub != prevSub) {
+                      showSub = curSub;
+                      prevSub = curSub;
+                    } else {
+                      showSub = "";
+                    }
+                    if (curDate != prevDate) {
+                      showDate = curDate;
+                      prevDate = curDate;
+                    } else {
+                      showDate = "";
+                    }
+                    if (curInvig != prevInvig) {
+                      showInvig = curInvig;
+                      prevInvig = curInvig;
+                    } else {
+                      showInvig = "";
+                    }
                     return (
                       <div className=" flex justify-around gap-[4rem]">
-                        <span>{subject}</span>
-                        <span>{subjectDate}</span>
-                        <span>{inviglatorId}</span>
-                        <span>{batch}</span>
+                        <span className="w-[5rem] font-semibold">
+                          {showSub}
+                        </span>
+                        <span className="w-[5rem]">{showDate}</span>
+                        <span className="w-[5rem]">{showInvig}</span>
+                        <span className="w-[5rem]">{batch}</span>
                         <span className="w-[10rem]">{arrOfStudents}</span>
                       </div>
                     );
@@ -319,8 +414,28 @@ const page = () => {
             <></>
           )}
         </div>
+        <div className="w-full h-1 border-b-gray-700 my-1 border-[1px] col-span-2"></div>
 
-        <button className="col-span-2" onClick={resetCLicked}>
+        <h4 className="col-span-2 text-xl font-semibold text-center">
+          Reset Schedule
+        </h4>
+        <label className="justify-cente flex items-center">
+          Enter Semester
+        </label>
+        <input
+          id="reset_schedule_sem"
+          name="reset_schedule_sem"
+          label="reset_schedule_sem"
+          type="number"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+          onBlur={formik.handleBlur}
+        />
+        <button
+          className="col-span-2 w-[15rem] items-center rounded-md p-2 font-semibold tracking-wider"
+          onClick={resetCLicked}
+          disabled={resetScheduleErr}
+        >
           Reset
         </button>
       </div>
